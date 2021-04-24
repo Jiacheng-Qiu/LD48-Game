2d1ac98f10a7e4edd57b7730d0587095c50c952c
rhit-qiuj1 <qiuj1@rose-hulman.edu>
2021-04-24 01:18:06 -0400

    Basic Mechanic

---
+++ Assets/Scripts/Player/InfinityPipe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Create new pipes if player reach the end of previous ones, and delete the earlier ones
+public class InfinityPipe : MonoBehaviour
+{
+    public int initAmount = 3;
+    public GameObject pipePrefab;
+    public GameObject player;
+
+    private ArrayList pipes;
+    private float angle;
+    private void Start()
+    {
+        pipes = new ArrayList();
+        angle = Mathf.Abs(pipePrefab.transform.rotation.z);
+        // Generate three pipes
+        for (int i = 0; i < initAmount; i++)
+        {
+            GenerateNewPipe();
+        }
+    }
+    void FixedUpdate()
+    {
+        // Delete passed pipes, and create new (check player position)
+        if (player.transform.position.x - ((GameObject)pipes[0]).transform.position.x > 17.7f)
+        {
+            Destroy((GameObject)pipes[0]);
+            pipes.RemoveAt(0);
+            GenerateNewPipe();
+        }
+    }
+
+    // Generate a new pipe based on previous position
+    private void GenerateNewPipe()
+    {
+        pipes.Add(Instantiate(pipePrefab));
+        int index = pipes.Count - 1;
+        if (index == 0)
+        {
+            ((GameObject)pipes[index]).transform.position = new Vector2(0, 0);
+        }
+        else
+        {
+            Vector2 prevPos = ((GameObject)pipes[index - 1]).transform.position;
+            ((GameObject)pipes[index]).transform.position = new Vector2(prevPos.x + 17.7f, prevPos.y - 8.255f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -0,0 +1,125 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private Rigidbody2D body;
+    private Animator playerAnimator;
+    public TextMeshProUGUI speedText;
+    public TextMeshProUGUI sizeText;
+
+    private float fixedSpeed; // Speed determined by size
+    public float flexibleSpeed = 6f;
+    public float jumpSpeed = 1200f;
+    private float currentSize;
+    public float speedConstant = 12f;
+
+    // For checking if character has already touched ground
+    public Transform groundCheck;
+    public LayerMask GroundLayer;
+    public bool isGround, isJump;
+    private bool jumpPressed;
+    void Start()
+    {
+        // Initial size = 0.8
+        currentSize = 0.8f;
+        body = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void UpdateCanvas()
+    {
+        speedText.text = body.velocity.x.ToString("F1");
+        sizeText.text = ((int)(currentSize * 100)).ToString() + "%";
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    private void UpdateSize()
+    {
+        // Check the moving of player, and change size
+        currentSize -= body.velocity.x * 0.0001f;
+        float size = Mathf.Clamp(currentSize * 2, 0.8f, 2f);
+        // Change size of player and collider size
+        transform.localScale = new Vector3(size, size, 1);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Change fixed speed based on size
+        fixedSpeed = currentSize * speedConstant;
+        // Check if is on ground by checking overlap with a layer
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, GroundLayer);
+        isJump = !isGround;
+
+        Move();
+        Jump();
+        
+        UpdateSize();
+        
+        UpdateCanvas();
+        // Check Gameover, TODO record data, and go to gameover scene + add rock collide
+        if (currentSize <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void Move()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        if (hInput != 0)
+        {
+            // TODO: change vector x comp next
+            body.velocity = new Vector2(fixedSpeed + hInput * flexibleSpeed, body.velocity.y);
+        } else
+        {
+            body.velocity = new Vector2(fixedSpeed, body.velocity.y);
+        }
+        body.AddForce(new Vector2(0, -50));
+    }
+
+    void Jump()
+    {
+        /*if (isGround)
+        {
+            // This is changing the animation of character into idle
+            playerAnimator.SetBool("Jumping", false);
+            playerAnimator.SetBool("Falling", false);
+        }
+        else if (body.velocity.y > 0)
+        {
+            // This is changing the animation of character into jumping
+            playerAnimator.SetBool("Jumping", true);
+            playerAnimator.SetBool("Falling", false);
+        }
+        else
+        {
+            // This is changing the animation of character into falling
+            playerAnimator.SetBool("Jumping", false);
+            playerAnimator.SetBool("Falling", true);
+        }*/
+
+        if (jumpPressed && isGround)
+        {
+            // Make jump verticle to the pipe by using tangent
+            float verticleJump = Mathf.Tan(Mathf.Abs(transform.rotation.z));
+            body.AddForce(new Vector2(verticleJump * jumpSpeed, jumpSpeed));
+            // body.velocity = new Vector2(body.velocity.x + verticleJump * Time.deltaTime, jumpSpeed * Time.deltaTime);
+            jumpPressed = false;
+        }
+    }
+
+}
