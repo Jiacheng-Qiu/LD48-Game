9478d6652a5705c5fce4d32cc41d16e8753adb66
rhit-qiuj1 <qiuj1@rose-hulman.edu>
2021-04-23 20:31:16 -0400

    RestartUI

--- Assets/Scripts/MenuUI/Buttons.cs
+++ Assets/Scripts/MenuUI/Buttons.cs
@@ -7,7 +7,17 @@ public class Buttons : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Test()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void Quit()
